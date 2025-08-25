f52a9fcfea7cfd7fd5fee88a116481d316e7cfa4
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-08-25 16:27:40 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// COMSC 210 |  Lab 3 | Andrew Tai
+// IDE used: VS Code
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    
+    return 0;
+}
